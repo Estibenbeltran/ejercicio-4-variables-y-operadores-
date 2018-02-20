8fe8db1d441b63d19eb7f5bae46c27eba2d3e0a1
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-02-19 23:08:33 -0500

    ejercicio 4 (variables y operaciones)
    hacer una ecuacion de segundo grado, con valores definidos final final

--- Ejercicio4Variablesyop/src/ejercicio4variablesyop/Ejercicio4Variablesyop.java
+++ Ejercicio4Variablesyop/src/ejercicio4variablesyop/Ejercicio4Variablesyop.java
@@ -14,10 +14,14 @@ package ejercicio4variablesyop;
  * 
  * este ejercicio permite resolver una ecuacion de segundo grado
  */
+/**
+ * 
+ * clase que contiene la operacion 
+ */
 class Operacion{
-    double a,b,par1,res;
+    double a,b,par1,res;//variables
     void op(int a, int b){
-        par1 = Math.pow(a,2);
+        par1 = Math.pow(a,2);//potenciacion de a
         res = (-(par1)-b*2);
     }
 }
@@ -28,12 +32,12 @@ public class Ejercicio4Variablesyop {
      */
     public static void main(String[] args) {
         Operacion ope = new Operacion();
-        int a=3, b=2;
+        int a=3, b=2;//se dan los valores definidos para la ecuacion
         System.out.println("La ecuacion a resolver es -a^2-2b=-13");
-        ope.op(a, b);
+        ope.op(a, b);//se llama la funcion para resolver la misma
         System.out.println("el valor de a es "+a);
         System.out.println("el valor de b es "+b);
-        System.out.println("La respuesta de la ecuacion es "+ope.res);
+        System.out.println("La respuesta de la ecuacion es "+ope.res);//respuesta final
     }
     
 }
